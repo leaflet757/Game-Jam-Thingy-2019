a37ff7e55401683f73a1b3d834da691bfbee7074
leaflet757 <leifmyer@gmail.com>
2019-01-26 14:17:11 -0800

    Added InitialVelocity script

---
+++ Game-Jam-Thingy-2019/Assets/scripts/InitialVelocity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialVelocity : MonoBehaviour 
+{
+    [SerializeField]
+    private Vector3 startingVelocity;
+
+	// Use this for initialization
+	void Start () 
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+		Debug.Assert(rb != null);
+
+        rb.AddForce(startingVelocity, ForceMode.Impulse);
+	}
+
+}
