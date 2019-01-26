c03f339b89d7fcbc596cbdfa0a986646a3a59dce
leaflet757 <leifmyer@gmail.com>
2019-01-26 10:50:15 -0800

    Added drag to player

--- Game-Jam-Thingy-2019/Assets/scripts/PlayerInput.cs
+++ Game-Jam-Thingy-2019/Assets/scripts/PlayerInput.cs
@@ -22,28 +22,38 @@ public class PlayerInput : MonoBehaviour
     [SerializeField]
     private float rotationSpeed = 50f;
 
+    [SerializeField]
+    private float dragAmount = 5f;
+
+    [SerializeField]
+    private GameObject playerBody;
+
     private Rigidbody playerRigidBody;
 
 	// Use this for initialization
 	void Start () 
     {
-		playerRigidBody = GetComponent<Rigidbody>();
+		playerRigidBody = playerBody.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
+        bool wasInputPressed = false;
+
 		Vector3 inputDirection = Vector3.zero;
         Vector3 eulerRotation = Vector3.zero;
 
         if (Input.GetKey(leanForward))
         {
             inputDirection += transform.forward;
+            wasInputPressed = true;
         }
 
         if (Input.GetKey(leanBackward))
         {
             inputDirection -= transform.forward;
+            wasInputPressed = true;
         }
 
         if (Input.GetKey(leanRight))
@@ -58,5 +68,14 @@ public class PlayerInput : MonoBehaviour
 
         playerRigidBody.AddForce(Time.deltaTime * moveSpeed * inputDirection.normalized, ForceMode.Force);
         transform.Rotate(Time.deltaTime * rotationSpeed * eulerRotation);
+
+        if (wasInputPressed)
+        {
+            playerRigidBody.drag = 0;
+        }
+        else
+        {
+            playerRigidBody.drag = dragAmount;
+        }
 	}
 }
