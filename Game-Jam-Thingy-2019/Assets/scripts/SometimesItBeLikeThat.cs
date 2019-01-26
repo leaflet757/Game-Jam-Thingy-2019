d348239e9baa90f1a1fce37dc224d3da2eda380f
leaflet757 <leifmyer@gmail.com>
2019-01-25 19:49:20 -0800

    Initial commit - setup unity project

---
+++ Game-Jam-Thingy-2019/Assets/scripts/SometimesItBeLikeThat.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SometimesItBeLikeThat : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Sometimes it be like that");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
