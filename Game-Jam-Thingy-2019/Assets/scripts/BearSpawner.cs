c915e5c91b9a94c5eb3d36f718d5c5b54a53a56b
James Webster <32810524+jawebste@users.noreply.github.com>
2019-01-27 10:56:45 -0800

    Added Models, Textures, Sounds, more road

---
+++ Game-Jam-Thingy-2019/Assets/scripts/BearSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearSpawner : MonoBehaviour {
+
+    public GameObject carPrefab = null;
+
+    private Rigidbody _rb; //do bears have animations or are they rigid?
+
+    // Use this for initialization
+    void Start()
+    {
+        Instantiate(carPrefab, transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
